10050e075340b53b90253b5d5ebbd7116aad5964
trendysloth <ww296@cornell.edu>
2020-07-08 17:55:12 -0700

    debugger

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
@@ -1,8 +1,11 @@
 package com.udacity.jwdnd.course1.cloudstorage.controllers;
 
 import com.udacity.jwdnd.course1.cloudstorage.models.User;
+import com.udacity.jwdnd.course1.cloudstorage.services.AuthenticationService;
+import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
 import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +16,17 @@ import javax.servlet.http.HttpSession;
 @Controller
 @RequestMapping("/home")
 public class HomeController {
+    @Autowired
+    private FileService fileService;
+
+    @Autowired
+    private UserService userService;
+
     @GetMapping()
-    public String homepageView() {
+    public String homepageView(Authentication authentication, Model model) {
+        String username = authentication.getName();
+        User user = userService.getUser(username);
+        model.addAttribute("files", fileService.getAllFiles(user.getUserid()));
         return "home";
     }
 }
